3c444b14515c7d878384e6eb2ab34112c7d983f3
Carlos Eduardo Chavez Rojas <114713651+chavezcar@users.noreply.github.com>
2024-03-03 21:11:25 -0500

    Ajuste a espacios
    Se ajustó el código en los espacios de los print de cada requerimiento.

--- PD_parte3_analiticaSimple.py
+++ PD_parte3_analiticaSimple.py
@@ -11,7 +11,7 @@ edades = np.array(data["age"])
 # Calcular el promedio de edad
 promedio_edad = np.mean(edades)
 
-print("\nEl promedio de edad de las personas participantes en el estudio es:", promedio_edad)
+print("El promedio de edad de las personas participantes en el estudio es:", promedio_edad,"\n")
 
 # Convertir el Dataset en un DataFrame de Pandas
 df = pd.DataFrame(data)
@@ -25,17 +25,17 @@ promedio_edad_fallecidos = df_fallecidos['age'].mean()
 promedio_edad_sobrevivientes = df_sobrevivientes['age'].mean()
 
 # Imprimir los promedios de edad
-print("\nPromedio de edad de fallecidos:", promedio_edad_fallecidos)
-print("Promedio de edad de sobrevivientes:", promedio_edad_sobrevivientes)
+print("Promedio de edad de fallecidos:", promedio_edad_fallecidos)
+print("Promedio de edad de sobrevivientes:", promedio_edad_sobrevivientes,"\n")
 
 # Verificar los tipos de datos en cada columna
 tipos_de_datos = df.dtypes
-print("\nTipos de datos en cada columna:")
-print(tipos_de_datos)
+print("Tipos de datos en cada columna:")
+print(tipos_de_datos,"\n")
 
 # Calcular la cantidad de hombres fumadores vs mujeres fumadoras
 cantidad_hombres_fumadores = df[(df['is_male'] == 1) & (df['is_smoker'] == 1)].shape[0]
 cantidad_mujeres_fumadoras = df[(df['is_male'] == 0) & (df['is_smoker'] == 1)].shape[0]
 
-print("\nCantidad de hombres fumadores:", cantidad_hombres_fumadores)
-print("Cantidad de mujeres fumadoras:", cantidad_mujeres_fumadoras)
+print("Cantidad de hombres fumadores:", cantidad_hombres_fumadores)
+print("Cantidad de mujeres fumadoras:", cantidad_mujeres_fumadoras,"\n")
